45d0682aeeb7cbfc971edd67756b72ad51036aeb
jill64 <intents.turrets0h@icloud.com>
2023-11-20 11:03:49 +0900

    embed sentry dsn

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,10 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { clientInit } from '@jill64/sentry-sveltekit-cloudflare'
 import { toast } from '@jill64/svelte-toast'
 import { get } from 'svelte/store'
 
-const onError = clientInit(PUBLIC_SENTRY_DSN)
+const onError = clientInit(
+  'https://1c68b59245ed2ac47ffde03021b408fd@o4505814639312896.ingest.sentry.io/4506143354912768'
+)
 
 export const handleError = onError((e) => {
   if (e.error instanceof Error) {
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,11 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
 import { onRender } from '@jill64/svelte-dark-theme'
 import { attach } from '@jill64/svelte-ogp'
 import { sequence } from '@sveltejs/kit/hooks'
 
-const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
+const { onHandle, onError } = serverInit(
+  'https://1c68b59245ed2ac47ffde03021b408fd@o4505814639312896.ingest.sentry.io/4506143354912768'
+)
 
 export const handle = onHandle(sequence(onRender(), attach))
 export const handleError = onError()
